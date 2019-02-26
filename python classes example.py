2ca68f9f0b9d3a3fcd59bd2db0265131c7572d22
saks <sakthivadivelv@gmail.com>
2019-02-26 16:50:02 +0530

    python classes examples

---
+++ python classes example.py
@@ -0,0 +1,20 @@
+
+class car():
+
+    wheel=5
+
+    def __init__(self):
+        self.brand='Toyoto'
+        self.type='zeep'
+        self.price=800000
+
+
+c1=car()
+c2=car()
+
+c1.type='SUV'
+car.wheel=3
+
+print(c1.brand,c1.type,c1.price,c1.wheel)
+
+print(c2.brand,c2.type,c2.price,c2.wheel)
