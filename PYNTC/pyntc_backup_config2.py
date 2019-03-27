70cad87db43339390f93428da23c2f9f89c68206
saks <sakthivadivelv@gmail.com>
2019-03-27 17:59:43 +0530

    PYNTC backing up the configurations using 4 lines of codes

---
+++ PYNTC/pyntc_backup_config2.py
@@ -0,0 +1,5 @@
+from pyntc import ntc_device as NTC
+
+iosvl2 = NTC(host='192.168.1.1', username='cisco', password='cisco', device_type='cisco_ios_ssh')
+iosvl2.open()
+iosvl2.backup_running_config('switch1.cfg')
