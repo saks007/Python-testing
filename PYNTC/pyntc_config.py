8ecd971dd728edbd0e7100c46ae6b12aa8d8f2d0
saks <sakthivadivelv@gmail.com>
2019-03-28 10:57:28 +0530

    PYNTC backing up the configurations - multiple devices - loop

--- PYNTC/pyntc_config.py
+++ PYNTC/pyntc_config.py
@@ -3,5 +3,8 @@ from pyntc import ntc_device as NTC
 iosvl2 = NTC(host='192.168.1.1', username='cisco', password='cisco', device_type='cisco_ios_ssh')
 iosvl2.open()
 
+# single command configuration
 iosvl2.config('hostname Switch1')
+
+# multiple commands configurations
 iosvl2.config_list(['hostname Sw1','int lo 10','ip add 20.20.20.1 255.255.255.0 '])
